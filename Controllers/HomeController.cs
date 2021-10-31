4eb25891411b405baaafc355547475fd653523cf
Selimcesur73@gmail.com <Selimcesur73@gmail.com>
2021-10-31 21:10:55 +0300

    Trying Database Connection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,16 +6,27 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Models;
+using Domain;
 
 namespace Forex_Rate_Monitoring.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        CurrentExchangeRate cer = new CurrentExchangeRate();
+        RatesDbContext ratesDbContext = new RatesDbContext();
 
         public HomeController(ILogger<HomeController> logger) {
             _logger = logger;
         }
 
-        public IActionResult Index() {
+        public IActionResult Index() {          
+            cer.Currencies = "asdas";
+            cer.ExchangeRate = 3.4;
+            cer.Date = new DateTime();
+
+            ratesDbContext.currentExchangeRates.Add(cer);
+            ratesDbContext.SaveChanges();
+
             return View();
         }
 
