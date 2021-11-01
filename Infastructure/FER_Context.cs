d41b4fbf2fe6f8fbaddda4dabd7271a5e738285c
SELIM CESUR <selim.cesur@ext.digiturk.com.tr>
2021-11-01 16:11:39 +0300

    Rearrange

---
+++ Infastructure/FER_Context.cs
@@ -0,0 +1,20 @@
+﻿using Application_Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infastructure
+{
+    public class FER_Context: DbContext
+    {
+        public FER_Context(): base()
+        {
+
+        }
+
+        public DbSet<ExchangeRateModel> CurrentExchangeRates { get; set; }
+
+    }
+}
