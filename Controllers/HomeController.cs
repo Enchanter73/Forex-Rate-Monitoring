bd484c3cc2cc485ae9a9a7dd422a2c61a19b35e6
SELIM CESUR <selim.cesur@ext.digiturk.com.tr>
2021-11-01 09:38:41 +0300

    Added API Key

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,28 +6,38 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Domain.Models;
-using Domain;
+using System.Text.Json;
+using System.Net;
+using System.Configuration;
 
 namespace Forex_Rate_Monitoring.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
-        CurrentExchangeRate cer = new CurrentExchangeRate();
-        RatesDbContext ratesDbContext = new RatesDbContext();
+
+        static string key = ConfigurationManager.AppSettings.Get("API-KEY");
+        
+        static string QUERY_URL = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=" + key;
+        Uri queryUri = new Uri(QUERY_URL);
 
         public HomeController(ILogger<HomeController> logger) {
             _logger = logger;
         }
 
-        public IActionResult Index() {          
-            cer.Currencies = "asdas";
-            cer.ExchangeRate = 3.4;
-            cer.Date = new DateTime();
+        public IActionResult Index() {
 
-            ratesDbContext.currentExchangeRates.Add(cer);
-            ratesDbContext.SaveChanges();
+            dynamic json_data = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(new WebClient().DownloadString(queryUri));
+            string fromCurrencyCode = "";
+            string toCurrencyCode = "";
+            string exchangeRate = "";
 
-            return View();
+            foreach (JsonElement element in json_data.Values)
+            {
+                fromCurrencyCode = element.GetProperty("1. From_Currency Code").ToString();
+                toCurrencyCode = element.GetProperty("3. To_Currency Code").ToString();
+                exchangeRate = element.GetProperty("5. Exchange Rate").ToString();
+            }
+
+            return View("Index", fromCurrencyCode);
         }
 
         public IActionResult Privacy() {
