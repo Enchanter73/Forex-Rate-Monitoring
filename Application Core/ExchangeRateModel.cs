8e76724f61e3c76b1cd17c78d7b38cbb42fe8502
SELIM CESUR <selim.cesur@ext.digiturk.com.tr>
2021-11-01 16:19:13 +0300

    Fixing files

---
+++ Application Core/ExchangeRateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application_Core
+{
+    public class ExchangeRateModel
+    {
+        public int ExchangeRateModelId { get; set; }
+        public string fromCurrencyCode { get; set; }
+        public string toCurrencyCode { get; set; }
+        public string ExchangeRate { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
